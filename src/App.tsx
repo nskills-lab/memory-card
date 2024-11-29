6c65bb9dbfa8e8dd8f4ceb5af5efe09d9f8c7a40
Nina <80556270+nskills-lab@users.noreply.github.com>
2024-11-28 18:24:56 -0600

    Add a way to keep of track of things to finish for this project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
 import './assets/styles/App.css';
 import { Board } from './components/Board';
 
+/**
+ * Steps:
+ * 1. Fetching of cards => done
+ * 2. Displaing of cards => done
+ * 3. Event listener that if  a user clicks, reshuffles the cards and displays them
+ * 4. Way to keep track of if a user has in the current game iteration clicked on this card
+ * 5. Way to keep the score
+ * 6. Way to restart the game
+ * 7. Way to flip cards: show the back of cards:
+ * - find a design for the back of the card
+ * - add sound that imitate the swooshing
+ * 8. Add short game instructions
+ * 7. Fix Typescript mistakes
+ * 8. Have the game published in github pages:
+ * - add github action to rebuild everytime i push to main branch
+ *
+ */
+
 function App() {
   return (
     <div id="game-container">
