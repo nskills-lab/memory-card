6ac3339ff28cb0d188df4cb2372b599a08704e07
Nina <80556270+nskills-lab@users.noreply.github.com>
2024-12-12 22:55:28 -0600

    Add a way to flip cards back and forth

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,11 +3,10 @@ import DeckOfCards from '../containers/deckOfCards';
 
 export function Card() {
   const [cards, updateCards] = useState([]);
+  const [flip, setFlip] = useState(false);
 
   // Runs one time when a component is mounted
   useEffect(() => {
-    console.log('Mounting');
-
     const fetchCards = async () => {
       DeckOfCards.deckId = await DeckOfCards.getNewDeck();
       const drawnCards = await DeckOfCards.drawCards(DeckOfCards.deckId);
@@ -18,43 +17,59 @@ export function Card() {
 
   useEffect(() => {
     const reshuffleCards = async () => {
+      console.log('Reshuffling');
       await DeckOfCards.reshuffleCards(DeckOfCards.deckId);
       const drawnCards = await DeckOfCards.drawCards(DeckOfCards.deckId);
       console.log(drawnCards);
       updateCards(drawnCards);
     };
 
-    const playSound = () => {
-      const audio = document.querySelector('#audio');
-      audio.play();
-    };
-
-    document.addEventListener('click', (e) => {
-      playSound();
-      if (e.target.matches('.card-front')) {
-        const cardElements = [...document.querySelectorAll('.card')];
-        cardElements.forEach((card) => {
-          const innerCard = card.querySelector('.card-inner');
-          innerCard.classList.toggle('card-flip');
-        });
-        reshuffleCards();
-
-        // return () => {
-        //   [...document.querySelectorAll('.card')].forEach((card) => {
-        //     const innerCard = card.querySelector('.card-inner');
-        //     innerCard.classList.remove('card-flip');
-        //   });
-        // };
+    const handler = (e) => {
+      if (e.target.matches('.card-front') || e.target.matches('.card-back')) {
+        const playSound = () => {
+          const audio = document.querySelector('#audio');
+          audio.play();
+        };
+        playSound();
+        handleClick();
+        if (DeckOfCards.deckId) {
+          reshuffleCards();
+        }
       }
-    });
+    };
+    document.addEventListener('click', handler);
+
+    return () => {
+      console.log('Cleaning up');
+      document.removeEventListener('click', handler);
+    };
   }, []);
+
+  function handleClick() {
+    console.log('Flipped');
+    const cardElements = [...document.querySelectorAll('.card')];
+    cardElements.forEach((card) => {
+      const innerCard = card.querySelector('.card-inner');
+      innerCard.classList.remove('card-reflip');
+      innerCard.classList.toggle('card-flip');
+    });
+  }
+
+  useEffect(() => {
+    const cardElements = [...document.querySelectorAll('.card')];
+    cardElements.forEach((card) => {
+      const innerCard = card.querySelector('.card-inner');
+      innerCard.classList.remove('card-flip');
+      innerCard.classList.toggle('card-reflip');
+    });
+  }, [cards]);
 
   return (
     <>
       {cards.map((card) => (
         <div className="card">
           <div className="card-inner">
-            <img className="card-front" src={card.images.png}></img>
+            <img className="card-front" src={card.image}></img>
             <img className="card-back"></img>
           </div>
         </div>
